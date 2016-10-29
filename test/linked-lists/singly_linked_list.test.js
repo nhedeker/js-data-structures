f6fdea5ce9f1d8e8784a012fab494d6fe2156ec6
Natasha Hedeker <Natasha.Hedeker@gmail.com>
2016-10-29 14:50:50 -0500

    refactored SinglyLinkedList & tests

--- test/linked-lists/singly_linked_list.test.js
+++ test/linked-lists/singly_linked_list.test.js
@@ -15,47 +15,47 @@ suite('Singly Linked List', () => {
   suite('Push', () => {
     test('Properly updates the length', () => {
       list.push(4);
-      assert.strictEqual(list.length, 1);
+      assert.strictEqual(list._length, 1);
 
       list.push(6);
-      assert.strictEqual(list.length, 2);
+      assert.strictEqual(list._length, 2);
 
       list.push(-1);
-      assert.strictEqual(list.length, 3);
+      assert.strictEqual(list._length, 3);
     });
 
     test('Properly updates the head', () => {
       list.push(5);
-      assert.strictEqual(list.head.val, 5);
-      assert.isNull(list.head.next);
+      assert.strictEqual(list._head.val, 5);
+      assert.isNull(list._head.next);
 
       list.push(3);
-      assert.strictEqual(list.head.val, 5);
-      assert.strictEqual(list.head.next.val, 3);
+      assert.strictEqual(list._head.val, 5);
+      assert.strictEqual(list._head.next.val, 3);
 
       list.push(9);
-      assert.strictEqual(list.head.val, 5);
-      assert.strictEqual(list.head.next.val, 3);
+      assert.strictEqual(list._head.val, 5);
+      assert.strictEqual(list._head.next.val, 3);
     });
 
     test('Properly updates the tail', () => {
       list.push(7);
-      assert.strictEqual(list.tail.val, 7);
-      assert.isNull(list.tail.next);
+      assert.strictEqual(list._tail.val, 7);
+      assert.isNull(list._tail.next);
 
       list.push(1);
-      assert.strictEqual(list.tail.val, 1);
-      assert.isNull(list.tail.next);
+      assert.strictEqual(list._tail.val, 1);
+      assert.isNull(list._tail.next);
 
       list.push(45);
-      assert.strictEqual(list.tail.val, 45);
-      assert.isNull(list.tail.next);
+      assert.strictEqual(list._tail.val, 45);
+      assert.isNull(list._tail.next);
     });
 
     test('Returns self so chaining works', () => {
-      assert.strictEqual(list.length, 0);
+      assert.strictEqual(list._length, 0);
       list.push(1).push(2);
-      assert.strictEqual(list.length, 2);
+      assert.strictEqual(list._length, 2);
     });
   });
 
@@ -77,107 +77,107 @@ suite('Singly Linked List', () => {
       list.push(5).push(6).push(7).push(8);
 
       list.pop();
-      assert.strictEqual(list.length, 3);
+      assert.strictEqual(list._length, 3);
 
       list.pop();
-      assert.strictEqual(list.length, 2);
+      assert.strictEqual(list._length, 2);
 
       list.pop();
-      assert.strictEqual(list.length, 1);
+      assert.strictEqual(list._length, 1);
 
       list.pop();
-      assert.strictEqual(list.length, 0);
+      assert.strictEqual(list._length, 0);
 
       list.pop();
-      assert.strictEqual(list.length, 0);
+      assert.strictEqual(list._length, 0);
     });
 
     test('Properly updates the tail', () => {
       list.push(5).push(6).push(7).push(8);
 
       list.pop();
-      assert.strictEqual(list.tail.val, 7);
+      assert.strictEqual(list._tail.val, 7);
 
       list.pop();
-      assert.strictEqual(list.tail.val, 6);
+      assert.strictEqual(list._tail.val, 6);
 
       list.pop();
-      assert.strictEqual(list.tail.val, 5);
+      assert.strictEqual(list._tail.val, 5);
 
       list.pop();
-      assert.isNull(list.tail);
+      assert.isNull(list._tail);
     });
 
     test('Properly updates the head', () => {
       list.push(1).push(2);
-      assert.strictEqual(list.head.val, 1);
+      assert.strictEqual(list._head.val, 1);
 
       list.pop();
-      assert.strictEqual(list.head.val, 1);
+      assert.strictEqual(list._head.val, 1);
 
       list.pop();
-      assert.isNull(list.head);
+      assert.isNull(list._head);
     });
   });
 
   suite('Unshift', () => {
     test('Properly updates the length', () => {
-      assert.strictEqual(list.length, 0);
+      assert.strictEqual(list._length, 0);
 
       list.unshift(5);
-      assert.strictEqual(list.length, 1);
+      assert.strictEqual(list._length, 1);
 
       list.unshift(8);
-      assert.strictEqual(list.length, 2);
+      assert.strictEqual(list._length, 2);
 
       list.unshift(2);
-      assert.strictEqual(list.length, 3);
+      assert.strictEqual(list._length, 3);
     });
 
     test('Properly updates the head', () => {
       list.unshift(6);
-      assert.strictEqual(list.head.val, 6);
-      assert.isNull(list.head.next);
+      assert.strictEqual(list._head.val, 6);
+      assert.isNull(list._head.next);
 
       list.unshift(1);
-      assert.strictEqual(list.head.val, 1);
-      assert.strictEqual(list.head.next.val, 6);
+      assert.strictEqual(list._head.val, 1);
+      assert.strictEqual(list._head.next.val, 6);
 
       list.unshift(9);
-      assert.strictEqual(list.head.val, 9);
-      assert.strictEqual(list.head.next.val, 1);
+      assert.strictEqual(list._head.val, 9);
+      assert.strictEqual(list._head.next.val, 1);
     });
 
     test('Properly updates the tail', () => {
       list.unshift(4);
-      assert.strictEqual(list.tail.val, 4);
-      assert.isNull(list.tail.next);
+      assert.strictEqual(list._tail.val, 4);
+      assert.isNull(list._tail.next);
 
       list.unshift(3);
-      assert.strictEqual(list.tail.val, 4);
-      assert.isNull(list.tail.next);
+      assert.strictEqual(list._tail.val, 4);
+      assert.isNull(list._tail.next);
 
       list.unshift(8);
-      assert.strictEqual(list.tail.val, 4);
-      assert.isNull(list.tail.next);
+      assert.strictEqual(list._tail.val, 4);
+      assert.isNull(list._tail.next);
     });
   });
 
   suite('Shift', () => {
     test('Removes a value from the front of the list', () => {
       list.push(4).push(2);
-      assert.strictEqual(list.length, 2);
+      assert.strictEqual(list._length, 2);
       assert.strictEqual(list.shift(), 4);
-      assert.strictEqual(list.length, 1);
+      assert.strictEqual(list._length, 1);
       assert.strictEqual(list.shift(), 2);
-      assert.strictEqual(list.length, 0);
+      assert.strictEqual(list._length, 0);
     });
   });
 
   suite('Get', () => {
     test('Gets a value from the list given an index', () => {
       list.push(5).push(6).push(7).push(8).push(9);
-      assert.strictEqual(list.length, 5);
+      assert.strictEqual(list._length, 5);
       assert.strictEqual(list.get(3), 8);
       assert.strictEqual(list.get(0), 5);
       assert.strictEqual(list.get(2), 7);
@@ -187,7 +187,7 @@ suite('Singly Linked List', () => {
   suite('Set', () => {
     test('Sets a value in the list given an index', () => {
       list.push(3).push(4).push(5).push(6).push(7);
-      assert.strictEqual(list.length, 5);
+      assert.strictEqual(list._length, 5);
 
       list.set(4, 76);
       assert.strictEqual(list.pop(), 76);
@@ -195,20 +195,20 @@ suite('Singly Linked List', () => {
       list.set(0, 67);
       assert.strictEqual(list.shift(), 67);
 
-      assert.strictEqual(list.length, 3);
+      assert.strictEqual(list._length, 3);
     });
   });
 
   suite('Remove', () => {
     test('Removes a value in the list given an index', () => {
       list.push(1).push(2).push(3).push(4).push(5);
-      assert.strictEqual(list.length, 5);
+      assert.strictEqual(list._length, 5);
       assert.strictEqual(list.remove(2), 3);
-      assert.strictEqual(list.length, 4);
+      assert.strictEqual(list._length, 4);
       assert.strictEqual(list.remove(0), 1);
-      assert.strictEqual(list.length, 3);
+      assert.strictEqual(list._length, 3);
       assert.strictEqual(list.remove(1), 4);
-      assert.strictEqual(list.length, 2);
+      assert.strictEqual(list._length, 2);
     });
   });
 });
