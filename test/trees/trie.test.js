ac382a4f6e18ac4c115e7382174fcb5b10e61393
Natasha Hedeker <Natasha.Hedeker@gmail.com>
2016-11-04 10:15:51 -0500

    started to add code to trie test

---
+++ test/trees/trie.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Trie = require('../../lib/trees/trie');
+const { assert } = require('chai');
+const { suite, test } = require('mocha');
+
+suite('Trie', () => {
+  let trie;
+});
