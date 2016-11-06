84ced2c72041f045bad9be41d079b14071aa2380
Natasha Hedeker <Natasha.Hedeker@gmail.com>
2016-11-06 09:16:54 -0600

    added test to learn trie suite

--- test/trees/trie.test.js
+++ test/trees/trie.test.js
@@ -6,4 +6,28 @@ const { suite, test } = require('mocha');
 
 suite('Trie', () => {
   let trie;
+
+  // eslint-disable-next-line no-undef
+  beforeEach(() => {
+    trie = new Trie();
+  });
+
+  suite('Learn', () => {
+    test('Learns a new word character by character', () => {
+      trie.learn('be');
+      const bChar = trie.characters.b;
+
+      assert.isNotNull(bChar);
+      assert.isNotNaN(bChar);
+      assert.isDefined(bChar);
+      assert.isNotOk(bChar.isWord);
+
+      const eChar = bChar.characters.e;
+
+      assert.isNotNaN(eChar);
+      assert.isDefined(eChar);
+      assert.isOk(eChar.isWord);
+      assert.deepEqual(eChar.characters, {});
+    });
+  });
 });
