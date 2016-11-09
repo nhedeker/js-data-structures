92db353f3f174dbd08ed3215f371ea774071b28e
Natasha Hedeker <Natasha.Hedeker@gmail.com>
2016-11-09 10:15:04 -0600

    created weighted graph & test files

---
+++ test/graphs/undirected_weighted_graph.js
@@ -0,0 +1,14 @@
+'use strict';
+
+require('../../lib/graphs/undirected_weighted_graph');
+const { assert } = require('chai');
+const { suite, test } = require('mocha');
+
+suite('Undirected Weighted Graph', () => {
+  let graph;
+
+  // eslint-disable-next-line no-undef
+  beforeEach(() => {
+    graph = new Graph();
+  });
+});
